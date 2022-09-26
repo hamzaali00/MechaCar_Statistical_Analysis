f7d73d81ffe5570e8729d5f41ea0fd1f6cdb3f23
hamzaali00 <100699289+hamzaali00@users.noreply.github.com>
2022-09-26 11:03:35 -0400

    Deliverable 2 Complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,5 @@
+# DELIVERABLE 1
+
 # Loading dplyr package
 library(dplyr)
 
@@ -10,4 +12,13 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 # Using summary() function to find p-value and r-squared value
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCarTable))
 
+# DELIVERABLE 2
 
+# Importing csv
+SuspensionCoil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# Create a summary dataframe
+total_summary <- SuspensionCoil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+# Create a summary grouped by Lot
+lot_summary <- SuspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
